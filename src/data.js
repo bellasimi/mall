b4d1302a0d898f33fede0e2fd7191796434d6efd
bellasimi <WorkSY1111@gmail.com>
2022-01-03 21:34:17 +0900

    [Front] 쇼핑몰 data js 분리

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id : device0,
+    title : "애플 노트북",
+    content : "nice laptop",
+    price : "1,000,000원"
+  },
+
+  {
+    id : device1,
+    title : "아이폰",
+    content : "high-end smartphone",
+    price : "1,200,000원"
+  },
+
+  {
+    id :device2,
+    title : "아이팟",
+    content : "multi mp3",
+    price : "200,000원"
+  }
+]
