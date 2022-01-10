9dc06fc7705251cbb25428aa4a93bb4a1b80365f
bellasimi <WorkSY1111@gmail.com>
2022-01-11 03:04:50 +0900

    [Front] 리덕스 reducer로 state quan 증감처리

--- src/index.js
+++ src/index.js
@@ -7,9 +7,33 @@ import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 
-let store = createStore(()=>{return [ {id : 0, name : "애플", quan : 2 }
-,{id : 1, name : "애플2", quan : 1 },{id : 2, name : "애플3", quan : 7 }
-] });
+
+let 기본값 = [
+{id : 0, name : "애플", quan : 2 },
+{id : 1, name : "애플2", quan : 1 },
+{id : 2, name : "애플3", quan : 7 }
+];
+
+/*  state가 될 변수 관리:삭제 */
+function reducer(state = 기본값, 디스패치){
+    let copy = [...state];
+    if(디스패치.type === "+"){
+        copy[디스패치.idx].quan++;
+        return copy
+
+    }else if( 디스패치.type === "-"){
+        if(copy[디스패치.idx].quan !=0){
+            copy[디스패치.idx].quan--;
+        }
+
+        return copy
+    }else{
+        return state
+    }
+}
+
+/* createStore() 거치면 변수가 state화 */
+let store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
